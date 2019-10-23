4a54127ac2cde09c5cd7c9a18976248b85e7eed5
Hema Tahir <56174762+Kurdistan2004@users.noreply.github.com>
2019-10-23 16:47:33 -0700

    Finished 2.3 section 4, Start 5

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-public GameObject player;
-
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +15,7 @@ public GameObject player;
     // Update is called once per frame
     void Update()
     {
-        
+        // Offset the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + new Vector3(0, 5, -7);
     }
 }
