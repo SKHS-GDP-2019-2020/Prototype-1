573630fbf406e035a5d741ab677463f71e7a77f4
Hema Tahir <56174762+Kurdistan2004@users.noreply.github.com>
2019-10-25 14:50:26 -0700

    Finished Lesson 3 start Lesson 4 next time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontallInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontallInput = Input.GetAxis("Horizontal");
+
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontallInput);
     }
 }
