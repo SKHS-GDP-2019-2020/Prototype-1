06954e58a60405d029993b3c74b578137d6f471f
Maximus Mercer <56046470+Merc4952@users.noreply.github.com>
2019-11-01 09:28:11 -0700

    fixed things
    it seems as tho it didnt save a few things i changed lsat time so i had to redo them

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -16,6 +16,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+     // offset the camera to make it follow the player 
          transform.position = player.transform.position + offset;
     }
 }
