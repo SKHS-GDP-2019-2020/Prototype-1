162d2ad3fa53f91a5b5fcc2a4c6fce74d1379d37
QuinnSuligoy <56174399+QuinnSuligoy@users.noreply.github.com>
2019-11-04 13:10:54 -0800

    Added a Boost Feature

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
+    public float StartingSpeed;
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float horizontalInput;
+    public GameObject Prop;
+    public float PropSpeed;
+    public float SpeedModifier;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +20,25 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        // modify the speed
+        speed = StartingSpeed;
+        speed = speed + (horizontalInput*SpeedModifier);
+
+        // get the user's 'Boost' input
+        horizontalInput = Input.GetAxis("Fire1");
+
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
-        // move the plane forward at a constant rate
+        // move the plane forward at a controlled speed 
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right, verticalInput * rotationSpeed * Time.deltaTime);
+
+        // rotate the prop
+        Prop.transform.Rotate(Vector3.forward * PropSpeed * Time.deltaTime);
     }
 }
